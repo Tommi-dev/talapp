ebfcd20019f7d0ac102f57859840f138ca2d6dc2
Tommi Mäkelä <tmmi.makela@outlook.com>
2021-02-26 11:44:14 +0200

    Kaksi radiobutton kysymyslohkoa lisätty

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -4,6 +4,125 @@ const Form = () => {
   return(
     <form className='form-container' >
       <h1>Kyselylomake</h1>
+
+      <section className='radiobutton-container' >
+        <label className='radiobutton-container-title' > <p>Kuinka motivoitunut tiimisi on?</p> </label>
+
+        <div className='radiobutton-subcontainer' >
+          <label className='radiobutton-subcontainer-container' >
+            <input 
+              type='radio'
+              name='motivation'
+            />
+            <span className='check' ></span>
+            Ei lainkaan motivoitunut
+          </label>
+        </div>
+
+        <div className='radiobutton-subcontainer' >
+          <label className='radiobutton-subcontainer-container' >
+            <input 
+              type='radio'
+              name='motivation'
+            />
+            <span className='check' ></span>
+            Ei kovin motivoitunut
+          </label>
+        </div>
+
+        <div className='radiobutton-subcontainer' >
+          <label className='radiobutton-subcontainer-container' >
+            <input 
+              type='radio'
+              name='motivation'
+            />
+            <span className='check' ></span>
+            Melko motivoitunut
+          </label>
+        </div>
+
+        <div className='radiobutton-subcontainer' >
+          <label className='radiobutton-subcontainer-container' >
+            <input 
+              type='radio'
+              name='motivation'
+            />
+            <span className='check' ></span>
+            Varsin motivoitunut
+          </label>
+        </div>
+
+        <div className='radiobutton-subcontainer' >
+          <label className='radiobutton-subcontainer-container' >
+            <input 
+              type='radio'
+              name='motivation'
+            />
+            <span className='check' ></span>
+            Erittäin motivoitunut
+          </label>
+        </div>
+      </section>
+
+      <section className='radiobutton-container' >
+        <label className='radiobutton-container-title' > <p>Kuinka hyvin henkilökemiat kohtaavat tiimissäsi?</p> </label>
+
+        <div className='radiobutton-subcontainer' >
+          <label className='radiobutton-subcontainer-container' >
+            <input 
+              type='radio'
+              name='chemistry'
+            />
+            <span className='check' ></span>
+            Ei lainkaan
+          </label>
+        </div>
+
+        <div className='radiobutton-subcontainer' >
+          <label className='radiobutton-subcontainer-container' >
+            <input 
+              type='radio'
+              name='chemistry'
+            />
+            <span className='check' ></span>
+            Ei kovin hyvin
+          </label>
+        </div>
+
+        <div className='radiobutton-subcontainer' >
+          <label className='radiobutton-subcontainer-container' >
+            <input 
+              type='radio'
+              name='chemistry'
+            />
+            <span className='check' ></span>
+            Melko hyvin
+          </label>
+        </div>
+
+        <div className='radiobutton-subcontainer' >
+          <label className='radiobutton-subcontainer-container' >
+            <input 
+              type='radio'
+              name='chemistry'
+            />
+            <span className='check' ></span>
+            Varsin hyvin
+          </label>
+        </div>
+
+        <div className='radiobutton-subcontainer' >
+          <label className='radiobutton-subcontainer-container' >
+            <input 
+              type='radio'
+              name='chemistry'
+            />
+            <span className='check' ></span>
+            Erittäin hyvin
+          </label>
+        </div>
+      </section>
+
     </form>
   )
 }
