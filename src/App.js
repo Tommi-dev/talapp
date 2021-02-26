cbea9d89c2ba39564b7153a22ca7ee4a2fd7d3a1
Tommi Mäkelä <tmmi.makela@outlook.com>
2021-02-26 11:03:03 +0200

    Lisätty ominaisuus palvelimella olevan datan hakemiseen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
+import teamService from './services/teams'
 
 const App = () => {
+  const [teams, setTeams] = useState([])
+
+  useEffect(() => {
+    teamService.getAll().then(data =>
+      setTeams(data)  
+    )
+  }, [])
+
+  console.log('teams length: ', teams.length)
+  console.log('teams: ', teams)
+  
   return (
     <div className="App">
       Hello world!
